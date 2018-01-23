b363769efcdfa7c75a892f04832136f2d276133c
jacksonarcade <jarcade1@gmail.com>
2018-01-23 17:15:11 -0500

    Keil project tutorial 1

--- main.c
+++ main.c
@@ -1,32 +1,57 @@
-#include "stm32f10x.h"
+#include<stm32f10x.h>
 
 int main(void)
 	
+
 {
+	//Enable GPIOC (peripheral clock) check RM0041 
+	//set bit 4 of RCC_APB2ENR register
 	
-	RCC->APB2ENR |= (1<<4);		//initialize the clk for GPIOC
-	
-	//Configure Pin 8 and Pin9 output with 50MHz
-	GPIOC -> CRH |= ((1<<0) | (1<<1) | (1<<4) |(1<<5) );
-	
-	
-	GPIOC -> CRH &= ~((1<<2) | (1<<3) | (1<<6) | (1<<7)); //general purpose output
+	RCC ->APB2ENR = 0x10;
 	
 	
+	//set GPIOC pin 8 and 9 as outputs
 	
+	GPIOC -> CRH = 0x33; //general purpose output pushpull ,, output mode with 50MHz frequency
 	
 	
 	while(1)
-		
 	{
-		GPIOC -> BSRR = ((1<<8)|(1<<9));
-		for(int i=0;i<5000000;i++);
-		GPIOC -> BSRR = (1<<(16+8)) | (1<<(16+9));
-		for(int i=0;i<5000000;i++);
+		
+		GPIOC -> BSRR = 0x300; //LED pins to high
+		for(int i =0;i<1000000;i++);  //Delay
+		
+		GPIOC -> BSRR = 0x3000000;  //LED pins to low
+		
+		for(int i =0;i<1000000;i++);  //Delay
+		
+		
 		
 	}
 	
 	
-	
+	//return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
